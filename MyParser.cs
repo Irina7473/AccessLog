fc2717c88323b4e50de70f97d1db121424b59de2
Irina <ten7473@mail.ru>
2024-04-14 19:29:40 +0500

    ready without passing parameters

--- MyParser.cs
+++ MyParser.cs
@@ -1,22 +1,27 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AccessLog
-{
-    class MyParser
-    {
-        //Дата в журнале доступа записана в формате: yyyy-MM-dd HH:mm:ss  now:u
-        //Даты в параметрах задаются в формате dd.MM.yyyy  now.ToString("d")
+{    
+    class MyParser : IValidatableObject
+    {         
         public DateOnly time_start;
         public DateOnly time_end;
         public string? address_start;
         public string? address_mask;
+        public string? address_end;
 
         public MyParser(DateOnly time_start, DateOnly time_end) 
         {
@@ -24,16 +29,40 @@ namespace AccessLog
             this.time_end = time_end;
         }
 
-        public MyParser(DateOnly time_start, DateOnly time_end, string address_start) :
+        public MyParser(DateOnly time_start, DateOnly time_end, string? address_start) :
             this(time_start, time_end)
         {           
             this.address_start = address_start;
         }
 
-        public MyParser(DateOnly time_start, DateOnly time_end, string address_start, string address_mask) :
+        public MyParser(DateOnly time_start, DateOnly time_end, string? address_start, string? address_mask) :
             this (time_start, time_end, address_start)
         {            
             this.address_mask = address_mask;
+            if (address_start!=null && address_mask!=null)
+                this.address_end = AddressEnd(address_start, address_mask);
+        }
+
+        private string AddressEnd(string address_start, string address_mask)
+        {            
+            UInt32 ipInt = AddressFromStringToInt(address_start);
+            UInt32 subnetMask = UInt32.MaxValue << (32 - int.Parse(address_mask));
+            UInt32 network = ipInt & subnetMask;
+            UInt32 broadcast = network | ~subnetMask;
+            UInt32 addressEnd = broadcast - 1;
+
+            return ipFromIntToString(addressEnd);
+        }
+
+        UInt32 AddressFromStringToInt(string address)
+        {
+            UInt32 addressInt = 0;
+            foreach (string part in address.Split('.'))
+            {
+                addressInt <<= 8;
+                addressInt |= UInt32.Parse(part);
+            }
+            return addressInt;
         }
 
 
@@ -48,11 +77,16 @@ namespace AccessLog
             {
                 int index1 = line.IndexOf(":");
                 int index2 = line.IndexOf(" ")-index1-1;
+
                 var address = line.Substring(0, index1);
+                //Проверка адреса
+                if (!ChekAddress(address)) continue;
+
                 var log_datetime = line.Substring(index1 + 1, index2);
                 DateOnly log_date;
                 if (DateOnly.TryParse(log_datetime, out log_date))
                 {
+                    //Проверка даты доступа
                     if (log_date >= time_start && log_date <= time_end)
                     {
                         if (requests.ContainsKey(address)) requests[address]++;
@@ -63,10 +97,78 @@ namespace AccessLog
             return requests;
         }
 
+        public string ipFromIntToString(UInt32 intValue)
+        {
+            return ((intValue >> 24) & 255).ToString() + "." + ((intValue >> 16) & 255).ToString() +
+                "." + ((intValue >> 8) & 255).ToString() + "." + (intValue & 255).ToString();
+        }
+
+        private bool ChekAddress(string address)
+        {
+            if (this.address_start == null) return true;
+            else
+            {
+                if (System.String.Compare(address, this.address_start) < 0) return false;
+                else {
+                if (this.address_mask == null || System.String.Compare(address, this.address_end) <= 0) return true;
+                else return false;
+                }
+            }            
+        }
+
         public static string ToDebugString(Dictionary<string, int> dictionary)
         {
             return string.Join("\n", dictionary.Select(kv => kv.Key + " - " + kv.Value).ToArray());
         }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+                        
+            if (time_start > time_end)
+                errors.Add(new ValidationResult("Нижняя граница временного интервала больше верхней"));
+
+            if (address_start!=null && !ValidateAddress(address_start))
+                errors.Add(new ValidationResult("Неправильно задана нижняя граница диапазона адресов"));
+
+            if (address_mask != null && !ValidateMask(address_mask))
+                errors.Add(new ValidationResult("Неправильно задана маска подсети"));
+
+            if (address_start==null && address_mask !=null)
+                errors.Add(new ValidationResult("Mаску подсети, задающую верхнюю границу диапазона, нельзя использовать, " +
+                    "если не задана нижняя граница диапазона адресов"));
+
+            return errors;
+        }
+                
+
+        private static bool ValidateMask(string mask)
+        {
+            int maskInt;
+            if (!int.TryParse(mask, out maskInt) || maskInt < 0 || maskInt > 32)
+                return false;
+
+            return true;
+        }
+
+        private static bool ValidateAddress(string address)
+        {
+            /*
+            Match match = Regex.Match(address, @"\b(\d{1,3}\.){3}\d{1,3}\b");
+            if (match.Success) return true;
+            else return false;
+            */
+
+            string[] ipParts = address.Split('.');
+            if (ipParts.Length != 4)
+                return false;
+            foreach (string partString in ipParts)
+            {
+                int partInt;
+                if (!int.TryParse(partString, out partInt) || partInt < 0 || partInt > 255)
+                    return false;
+            }
+            return true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,34 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AccessLog;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
 
 Console.WriteLine("Журнал доступа");
 
-var time_start = DateOnly.Parse("23.01.2024");
-var time_end = new DateOnly(2024, 01, 24);
-//var time_start = new DateTime(2024, 01, 25);
-//var time_end = new DateTime(2024, 01, 25);
-Console.WriteLine(time_start.ToString());
-Console.WriteLine(time_end.ToString());
-//string? address_start;
-//string? address_mask;
-
-
 var file1 = new TextFile("--file-log");
-/*
-file1.WriteingToFile("192.168.0.103:2024-01-23 08:10:00");
-file1.WriteingToFile("192.168.0.107:2024-01-23 10:10:00");
-file1.WriteingToFile("192.168.0.103:2024-01-24 10:15:00");
-file1.WriteingToFile("192.168.0.107:2024-01-25 09:10:00");
-file1.WriteingToFile("192.168.0.103:2024-01-25 09:40:00");
-file1.WriteingToFile("192.168.0.106:2024-01-23 09:40:00");
-file1.WriteingToFile("192.168.0.106:2024-01-24 10:40:00");
-*/
 Console.WriteLine(file1.ReadingFromFile());
-
 var file2 = new TextFile("--file-output");
 
-MyParser parser1 = new(time_start, time_end);
+var time_start = DateOnly.Parse("25.01.2024");
+var time_end = DateOnly.Parse("25.01.2024");
+string? address_start = "196.68.1.104";
+string? address_mask = "27";
+
+MyParser parser1 = new(time_start, time_end, address_start, address_mask);
 var requests = parser1.Parser(file1.ReadingFromFile());
 var data = MyParser.ToDebugString(requests);
+data = "За период " + time_start + "-" + time_end + "\nзафиксировано количество обращений с IP-адресов :\n" + data;
 file2.WriteingToFile(data);
 Console.WriteLine(file2.ReadingFromFile());
